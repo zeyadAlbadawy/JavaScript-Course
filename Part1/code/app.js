fc5a3cdb144ec30aad8bea2c877fa7a4d1e05dd5
zeyad albadawy <99131483+zeyadAlbadawy@users.noreply.github.com>
2024-06-18 13:36:30 +0300

    code updated

---
+++ Part1/code/app.js
@@ -0,0 +1,43 @@
+// alert("Hi EveryOne !");
+ 
+// == Vs === 
+
+// const age = 18;
+// if(age == '18')
+//     console.log("You are an adult !");
+
+// if(age === 18)
+//     console.log("You Are An Adult ! ");
+
+
+// Boolean Operators
+// const hasDrivenLicense = true;
+// const hasGoodVision = false;
+
+// console.log(!hasDrivenLicense);
+
+// const shouldDrive = hasDrivenLicense && hasGoodVision;
+// if(shouldDrive)
+//     console.log('She is able to drive');
+// else 
+//   console.log("She is not ablt to drive !");
+
+
+// The Switch Statement
+// const day = 'monday';
+// switch(day) {
+//     case 'monday':
+//         console.log("I Must check my email");
+//     break;
+//     case 'friday':
+//         console.log("Today is Friday ! it is free");
+//     break;
+//     default: 
+//     console.log("Go to your Work !");
+// }
+
+
+// Ternary Operators
+const age = 20;
+const meal = age >= 18 ? 'breakfast' : 'dinner ';
+console.log(meal);
---
+++ Part1/code/task1Sol.js
@@ -0,0 +1,5 @@
+let country = "Egypt";
+let continent = "Africa";
+let population = 120000000;
+const fullCv = `I'm zeyad, I'm from ${country}, a 20 years old engineering student ! `;
+console.log(fullCv);
